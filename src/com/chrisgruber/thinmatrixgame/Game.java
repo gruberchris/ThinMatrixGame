42643b1a581e95ea369bef51eee6670f0d6c5502
Chris Gruber <chris@chrisgruber.com>
2019-09-27 00:28:03 -0500

    Tutorial 8: Use cube model. Triangles render randomly as opengl hasn't been told in which order they out to be rendered. Makes for a cool effect!

--- src/com/chrisgruber/thinmatrixgame/Game.java
+++ src/com/chrisgruber/thinmatrixgame/Game.java
@@ -33,6 +33,7 @@ public class Game implements Runnable {
 
         Renderer renderer = new Renderer(staticShader);
 
+        /*
         float[] modelVertices = {
                 -0.5f, 0.5f, 0f,        // V0
                 -0.5f, -0.5f, 0f,       // V1
@@ -50,6 +51,85 @@ public class Game implements Runnable {
                 0, 1,                   // V1
                 1, 1,                   // V2
                 1, 0                    // V3
+        };
+        */
+
+        float[] modelVertices = {
+                -0.5f,0.5f,-0.5f,
+                -0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,-0.5f,
+                0.5f,0.5f,-0.5f,
+
+                -0.5f,0.5f,0.5f,
+                -0.5f,-0.5f,0.5f,
+                0.5f,-0.5f,0.5f,
+                0.5f,0.5f,0.5f,
+
+                0.5f,0.5f,-0.5f,
+                0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,0.5f,
+                0.5f,0.5f,0.5f,
+
+                -0.5f,0.5f,-0.5f,
+                -0.5f,-0.5f,-0.5f,
+                -0.5f,-0.5f,0.5f,
+                -0.5f,0.5f,0.5f,
+
+                -0.5f,0.5f,0.5f,
+                -0.5f,0.5f,-0.5f,
+                0.5f,0.5f,-0.5f,
+                0.5f,0.5f,0.5f,
+
+                -0.5f,-0.5f,0.5f,
+                -0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,0.5f
+
+        };
+
+        float[] textureCoords = {
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0
+
+
+        };
+
+        int[] modelIndices = {
+                0,1,3,
+                3,1,2,
+                4,5,7,
+                7,5,6,
+                8,9,11,
+                11,9,10,
+                12,13,15,
+                15,13,14,
+                16,17,19,
+                19,17,18,
+                20,21,23,
+                23,21,22
+
         };
 
         RawModel model = modelLoader.loadToVao(modelVertices, textureCoords, modelIndices);
