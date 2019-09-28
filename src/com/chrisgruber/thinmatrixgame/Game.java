c8a4ee2312f19a94c6679b18ea153a9dbfbf075d
Chris Gruber <chris@chrisgruber.com>
2019-09-28 01:21:24 -0500

    Tutorial 9 & 10: Parsing Wavefront .OBJ format and rendering 3D models

--- src/com/chrisgruber/thinmatrixgame/Game.java
+++ src/com/chrisgruber/thinmatrixgame/Game.java
@@ -2,6 +2,7 @@ package com.chrisgruber.thinmatrixgame;
 
 import com.chrisgruber.thinmatrixgame.engine.DisplayManager;
 import com.chrisgruber.thinmatrixgame.engine.ModelLoader;
+import com.chrisgruber.thinmatrixgame.engine.ObjLoader;
 import com.chrisgruber.thinmatrixgame.engine.entities.Camera;
 import com.chrisgruber.thinmatrixgame.engine.entities.Entity;
 import com.chrisgruber.thinmatrixgame.engine.models.RawModel;
@@ -27,121 +28,20 @@ public class Game implements Runnable {
         System.out.println("OpenGL: " + DisplayManager.getOpenGlVersionMessage());
 
         ModelLoader modelLoader = new ModelLoader();
-
         StaticShader staticShader = new StaticShader();
         staticShader.create();
-
         Renderer renderer = new Renderer(staticShader);
 
-        /*
-        float[] modelVertices = {
-                -0.5f, 0.5f, 0f,        // V0
-                -0.5f, -0.5f, 0f,       // V1
-                0.5f, -0.5f, 0f,        // V2
-                0.5f, 0.5f, 0f          // V3
-        };
-
-        int[] modelIndices = {
-                0, 1, 3,                // Top left triangle (V0, V1, V3)
-                3, 1, 2                 // Bottom right triangle (V3, V1, V2)
-        };
-
-        float[] textureCoords = {
-                0, 0,                   // V0
-                0, 1,                   // V1
-                1, 1,                   // V2
-                1, 0                    // V3
-        };
-        */
-
-        float[] modelVertices = {
-                -0.5f,0.5f,-0.5f,
-                -0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,-0.5f,
-                0.5f,0.5f,-0.5f,
-
-                -0.5f,0.5f,0.5f,
-                -0.5f,-0.5f,0.5f,
-                0.5f,-0.5f,0.5f,
-                0.5f,0.5f,0.5f,
-
-                0.5f,0.5f,-0.5f,
-                0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,0.5f,
-                0.5f,0.5f,0.5f,
-
-                -0.5f,0.5f,-0.5f,
-                -0.5f,-0.5f,-0.5f,
-                -0.5f,-0.5f,0.5f,
-                -0.5f,0.5f,0.5f,
-
-                -0.5f,0.5f,0.5f,
-                -0.5f,0.5f,-0.5f,
-                0.5f,0.5f,-0.5f,
-                0.5f,0.5f,0.5f,
-
-                -0.5f,-0.5f,0.5f,
-                -0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,0.5f
-
-        };
-
-        float[] textureCoords = {
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0
-
-
-        };
-
-        int[] modelIndices = {
-                0,1,3,
-                3,1,2,
-                4,5,7,
-                7,5,6,
-                8,9,11,
-                11,9,10,
-                12,13,15,
-                15,13,14,
-                16,17,19,
-                19,17,18,
-                20,21,23,
-                23,21,22
-
-        };
-
-        RawModel model = modelLoader.loadToVao(modelVertices, textureCoords, modelIndices);
-        ModelTexture modelTexture = new ModelTexture(modelLoader.loadTexture("resources/theman.png"));
+        RawModel model = ObjLoader.loadObjModel("resources/stall.obj", modelLoader);
+        ModelTexture modelTexture = new ModelTexture(modelLoader.loadTexture("resources/stallTexture.png"));
         TexturedModel texturedModel = new TexturedModel(model, modelTexture);
-        Entity entity = new Entity(texturedModel, new Vector3f(0, 0, -5), 0, 0, 0, 1);
+        Entity entity = new Entity(texturedModel, new Vector3f(0, -2.5f, -15), 0, 0, 0, 1);
         Camera camera = new Camera();
 
         while(DisplayManager.shouldDisplayClose()) {
             // move the entity and rotate each frame
-            //entity.increasePosition(0, 0, -0.1f);
-            entity.increaseRotation(1, 1, 0);
+            // entity.increasePosition(0, 0, -0.1f);
+            entity.increaseRotation(0, 1, 0);
 
             camera.move();
 
---
+++ src/com/chrisgruber/thinmatrixgame/engine/ObjLoader.java
@@ -0,0 +1,113 @@
+package com.chrisgruber.thinmatrixgame.engine;
+
+import com.chrisgruber.thinmatrixgame.engine.models.RawModel;
+import org.joml.Vector2f;
+import org.joml.Vector3f;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObjLoader {
+    private ObjLoader() {}
+
+    public static RawModel loadObjModel(String filename, ModelLoader modelLoader) {
+        FileReader fileReader = null;
+
+        try {
+            fileReader = new FileReader(new File(filename));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Unable to load file: " + filename);
+        }
+
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String line;
+        List<Vector3f> vertices = new ArrayList<>();
+        List<Vector2f> textures = new ArrayList<>();
+        List<Vector3f> normals = new ArrayList<>();
+        List<Integer> indices = new ArrayList<>();
+        float[] verticesArray = null;
+        float[] texturesArray = null;
+        float[] normalsArray = null;
+        int[] indicesArray = null;
+
+        try {
+            while(true) {
+                line = bufferedReader.readLine();
+                String[] currentLine = line.split(" ");
+
+                if (line.startsWith("v ")) {
+                    Vector3f vertex = new Vector3f(Float.parseFloat(currentLine[1]), Float.parseFloat(currentLine[2]), Float.parseFloat(currentLine[3]));
+                    vertices.add(vertex);
+                } else if (line.startsWith("vt ")) {
+                    Vector2f texture = new Vector2f(Float.parseFloat(currentLine[1]), Float.parseFloat(currentLine[2]));
+                    textures.add(texture);
+                } else if (line.startsWith("vn ")) {
+                    Vector3f normal = new Vector3f(Float.parseFloat(currentLine[1]), Float.parseFloat(currentLine[2]), Float.parseFloat(currentLine[3]));
+                    normals.add(normal);
+                } else if (line.startsWith("f ")) {
+                    texturesArray = new float[vertices.size() * 2];
+                    normalsArray = new float[vertices.size() * 3];
+                    break;
+                }
+            }
+
+            while (line != null) {
+                if (!line.startsWith("f ")) {
+                    line = bufferedReader.readLine();
+                    continue;
+                }
+
+                String[] currentLine = line.split(" ");
+                String[] vertex1 = currentLine[1].split("/");
+                String[] vertex2 = currentLine[2].split("/");
+                String[] vertex3 = currentLine[3].split("/");
+
+                processVertex(vertex1, indices, textures, normals, texturesArray, normalsArray);
+                processVertex(vertex2, indices, textures, normals, texturesArray, normalsArray);
+                processVertex(vertex3, indices, textures, normals, texturesArray, normalsArray);
+
+                line = bufferedReader.readLine();
+            }
+
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        verticesArray = new float[vertices.size() * 3];
+        indicesArray = new int[indices.size()];
+
+        int vertexPointer = 0;
+
+        for (Vector3f vertex : vertices) {
+            verticesArray[vertexPointer++] = vertex.x;
+            verticesArray[vertexPointer++] = vertex.y;
+            verticesArray[vertexPointer++] = vertex.z;
+        }
+
+        for (int i = 0; i < indices.size(); i++) {
+            indicesArray[i] = indices.get(i);
+        }
+
+        return modelLoader.loadToVao(verticesArray, texturesArray, indicesArray);
+    }
+
+    private static void processVertex(String[] vertexData, List<Integer> indices, List<Vector2f> textures, List<Vector3f> normals, float[] textureArray, float[] normalsArray) {
+        int currentVertexPointer = Integer.parseInt(vertexData[0]) - 1;
+
+        indices.add(currentVertexPointer);
+
+        Vector2f currentTexture = textures.get(Integer.parseInt(vertexData[1]) - 1);
+
+        textureArray[currentVertexPointer * 2] = currentTexture.x;
+        textureArray[currentVertexPointer * 2 + 1] = 1 - currentTexture.y;
+
+        Vector3f currentNormal = normals.get(Integer.parseInt(vertexData[2]) - 1);
+
+        normalsArray[currentVertexPointer * 3] = currentNormal.x;
+        normalsArray[currentVertexPointer * 3 + 1] = currentNormal.y;
+        normalsArray[currentVertexPointer * 3 + 2] = currentNormal.z;
+    }
+}
