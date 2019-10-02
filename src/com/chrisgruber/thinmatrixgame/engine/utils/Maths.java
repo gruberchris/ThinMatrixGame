053a448d602f6775b33136aabc45f0e4a5e5b942
Chris Gruber <chris@chrisgruber.com>
2019-10-01 23:04:51 -0500

    implement calculating height of a triangle at position using the Barycentric equation

--- src/com/chrisgruber/thinmatrixgame/engine/utils/Maths.java
+++ src/com/chrisgruber/thinmatrixgame/engine/utils/Maths.java
@@ -2,6 +2,7 @@ package com.chrisgruber.thinmatrixgame.engine.utils;
 
 import com.chrisgruber.thinmatrixgame.engine.entities.Camera;
 import org.joml.Matrix4f;
+import org.joml.Vector2f;
 import org.joml.Vector3f;
 
 public class Maths {
@@ -30,5 +31,13 @@ public class Maths {
         viewMatrix.translate(negativeCameraPos);
 
         return viewMatrix;
+    }
+
+    public static float calculateTriangleHeightByBarycentric(Vector3f p1, Vector3f p2, Vector3f p3, Vector2f pos) {
+        float det = (p2.z - p3.z) * (p1.x - p3.x) + (p3.x - p2.x) * (p1.z - p3.z);
+        float l1 = ((p2.z - p3.z) * (pos.x - p3.x) + (p3.x - p2.x) * (pos.y - p3.z)) / det;
+        float l2 = ((p3.z - p1.z) * (pos.x - p3.x) + (p1.x - p3.x) * (pos.y - p3.z)) / det;
+        float l3 = 1.0f - l1 - l2;
+        return l1 * p1.y + l2 * p2.y + l3 * p3.y;   // returns height of the triangle at "pos" position
     }
 }
